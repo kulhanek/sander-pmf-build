f891ebe4861da8ff591024497dbd0ef7f262c95f
Petr Kulhanek <kulhanek@chemi.muni.cz>
2015-12-13 14:48:05 +0100

    build update

--- 08.git-status.bat
+++ 08.git-status.bat
@@ -21,18 +21,13 @@ REM -----------------------------------------------------------------
     set P=%1
     set P=%P:/=\%
     set R=%2
-	echo. 
- 	echo ^# %R% -^> %P%
-	echo ^# -------------------------------------------
-	set OLDPWD=%cd%
-	if NOT EXIST %P% mkdir %P% || exit /B 1
+    echo. 
+    echo ^# %R% -^> %P%
+    echo ^# -------------------------------------------
+    set OLDPWD=%cd%
     cd %P% || exit /B 1
-    if NOT EXIST .git (
-   		git init
-   		git remote add github https://github.com/kulhanek/%R%.git
- 	)
-	git pull github master || exit /B 1
-	cd %OLDPWD%
+    git status || exit /B 1
+    cd %OLDPWD%
 goto:EOF
 
 
